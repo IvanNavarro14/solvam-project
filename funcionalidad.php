a5edf8077d22ec47145bdf0e5956f8e841d7700e
Ivan <valencianistaivan@gmail.com>
2024-01-11 09:47:03 +0100

    Sexto commit

--- funcionalidad.php
+++ funcionalidad.php
@@ -1,11 +1,2 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Funcionalidad</title>
-</head>
-<body>
-    <h1>Funcionalidad php</h1>
-</body>
-</html>
+<?php:
+echo("Gracias Dios");
